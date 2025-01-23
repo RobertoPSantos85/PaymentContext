eae907621ac78dbd305597eb2e5f873554045cb0
Roberto <robertopsantos85@gmail.com>
2025-01-23 20:36:22 -0300

    Adicionado as entidades boleto, creditcard, paypal

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests.Entities
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student("Zé", "Ruela", "123456789", "ze.ruela@provedor.com");
+            student.AddSubscription(subscription);
+        }
+    }
+}
